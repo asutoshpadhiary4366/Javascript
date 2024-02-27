100ef809ba466cb8dec5adcd99a0890b30ebc913
asutoshpadhiary4366 <102223076+asutoshpadhiary4366@users.noreply.github.com>
2024-02-28 01:24:01 +0530

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+  console.log(button);
+  button.addEventListener('mouseover', (e) => {
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'white':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'yellow':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'blue':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'purple':
+        body.style.backgroundColor = e.target.id;
+        break;
+    }
+  });
+});
---
+++ 6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,23 @@
+const randomColor = function () {
+  const hex = '0123456789ABCDE';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangingColor = () => {
+  changebgColor = () => {
+    document.body.style.backgroundColor = randomColor();
+  };
+  if (!intervalId) {
+    intervalId = setInterval(changebgColor, 1000);
+  }
+};
+const stopChangingColor = () => {
+  clearTimeout(intervalId);
+  intervalId = null;
+};
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
